947ded0505ec9479807a99aa48fe368eec28766c
github username <github email address>
2017-09-29 14:41:06 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,7 @@
-# Enter your procedural solution here!
+def collect_multiples(num)
+  (1..(num - 1)).select{|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
